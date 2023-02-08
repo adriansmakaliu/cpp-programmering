0417909783436f55af99fe087b0ca12152551c14
adrsm067 <adrsm067@su17-116.ad.liu.se>
2023-02-08 10:52:52 +0100

    First push

---
+++ Uppgift-1-Klockslag/iomanip test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    int number = 256;
+    cout << setw(8) << setfill('0') << number << endl;
+
+
+}
---
+++ Uppgift-2-Lista/list.h
@@ -0,0 +1,19 @@
+class List
+{
+public:
+  List();
+  void insert(int val);
+  void delete(int val);
+   int get(int index) const;
+   int size() const;
+
+private:
+  class Element
+  {
+    int value;
+    Element* next;
+    Element* prev;
+  }
+    Element* head;
+    Element* tail;
+  }
